07bf6849756dc9f7ff350ede91b5c61e82536b22
Theonilah Owali <theonilahtash@gmail.com>
2018-11-18 19:18:42 +0300

    add a registration and a login form for the users

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+from wtforms import ValidationError
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Enter a valid Email address', validators=[Required(),Email()])
+    username = StringField('Enter your username', validators=[Required()])
+    password = PasswordField('Password', validators=[Required(),
+    EqualTo('password_confirm', message = 'Password must match')])
+    password_confirm = PasswordField('Confirm Password', validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken try a new one')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('LogIn')
