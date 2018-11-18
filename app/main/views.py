99f4b7b094426c2fb39ad66f3d1ac76890257a36
Theonilah Owali <theonilahtash@gmail.com>
2018-11-18 19:27:21 +0300

    add a function that returns the index page and its data

---
+++ app/main/views.py
@@ -0,0 +1,42 @@
+
+from flask import render_template, url_for, flash, redirect, request, abort
+from . import main
+from flask_login import login_required, current_user
+from .forms import PitchForm, CommentForm
+from ..models import User, Category, Pitch, Comment
+from ..import db
+
+@main.route('/')
+def index():
+    """
+    Function that returns the index page
+    """
+    return render_template('index.html')
+
+@main.route('/pitch/new', methods=["GET", "POST"])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        pitch = Pitch(title = form.title.data, body = form.body.data)
+        db.session.add(pitch)
+        db.session.commit()
+        flash('Your pitch has been created succesfully')
+        return redirect(url_for('main.new_pitch'))
+    title = "Create a Pitch"
+    pitches = Pitch.query.all()
+
+    return render_template('pitch.html', title=title, form=form, pitch_list=pitches)
+
+@main.route('/comment/new', methods = ["GET", "POST"])
+@login_required
+def new_comment():
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = Comment(comment=comment_form.comment.data)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been posted succesfully')
+        return redirect(url_for('main.new_comment'))
+    comments = Comment.query.all()
+    return render_template('form.html', comment_form=comment_form, comment_list=comments)
