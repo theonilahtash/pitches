16b5317f84a0e184ca5c86f71f4485869d982bc3
Theonilah Owali <theonilahtash@gmail.com>
2018-11-19 11:55:28 +0300

    add form for users to choose the category they would like to pitch on then add their pitch and submit

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,9 +6,9 @@ from wtforms.validators import DataRequired
 
 class PitchForm(FlaskForm):
     title = StringField('Pitch Category Title', validators=[DataRequired()])
-    body = TextAreaField('Enter your Pitch here', validators=[DataRequired()])
+    body = TextAreaField('Enter your Pitch', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Write a comment here', validators=[DataRequired()])
+    comment = TextAreaField('Add a comment', validators=[DataRequired()])
     submit = SubmitField('Submit')
