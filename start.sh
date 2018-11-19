714d7c67595d4dd4cde17193de42a19f718ebf03
Theonilah Owali <theonilahtash@gmail.com>
2018-11-19 12:20:37 +0300

    rectified errors in my application

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export SECRET_KEY="theO@1234"
-export DATABASE_URL='postgres://hagmrftcqixigw:b6d7f942bd317cb9525541a107944e16227f0929c6d2df1beefafb467ef3f066@ec2-54-163-236-188.compute-1.amazonaws.com:5432/dcurnu29299lnb' 
+export SECRET_KEY=TheO.1234
+
 python3.6 manage.py server
