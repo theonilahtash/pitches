156bef064601672713488135ae59145ce105fe2f
Theonilah Owali <theonilahtash@gmail.com>
2018-11-19 11:50:51 +0300

    edited my sqlalchemy database uri to make it responsive

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elvis:elvis4122@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/pitch'
 
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/pitch'
     DEBUG = True
 
 config_options = {
